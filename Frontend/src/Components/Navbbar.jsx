f05832316c4c309b79883fb8149c528260085630
AryanBhoi0005 <aryanbhoi1962@gmail.com>
2024-09-26 23:05:20 +0530

    .

--- Frontend/src/Components/Navbbar.jsx
+++ Frontend/src/Components/Navbbar.jsx
@@ -37,10 +37,10 @@ function Navbbar() {
    
     const navComponents=(
         <>
-      <li className='dark:hover:bg-yellow-500 dark:hover:rounded-xl dark:font-bold hover:bg-yellow-500 hover:rounded-xl'><a href="/">Home</a></li>
-      <li className='dark:hover:bg-yellow-500 dark:hover:rounded-xl dark:font-bold hover:bg-yellow-500 hover:rounded-xl'><a href="/Course">Books</a></li>
-      <li className='dark:hover:bg-yellow-500 dark:hover:rounded-xl dark:font-bold hover:bg-yellow-500 hover:rounded-xl'><a href='/Contact'>Contact</a></li>
-      <li className='dark:hover:bg-yellow-500 dark:hover:rounded-xl dark:font-bold hover:bg-yellow-500 hover:rounded-xl'><a href='/About'>About</a></li>
+      <li className='dark:hover:bg-yellow-500 dark:hover:rounded-xl dark:font-bold hover:bg-yellow-500 hover:rounded-xl'><Link>"/"</Link></li>
+      <li className='dark:hover:bg-yellow-500 dark:hover:rounded-xl dark:font-bold hover:bg-yellow-500 hover:rounded-xl'><Link>"/Course"</Link></li>
+      <li className='dark:hover:bg-yellow-500 dark:hover:rounded-xl dark:font-bold hover:bg-yellow-500 hover:rounded-xl'><Link>"/Contact"</Link></li>
+      <li className='dark:hover:bg-yellow-500 dark:hover:rounded-xl dark:font-bold hover:bg-yellow-500 hover:rounded-xl'><Link>"/About"</Link></li>
         </>
     )
 
