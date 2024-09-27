c30362e74803c8b60f80fad8f5e13b9095b95052
AryanBhoi0005 <aryanbhoi1962@gmail.com>
2024-09-27 12:32:33 +0530

    Changes

--- Frontend/src/Components/Banner.jsx
+++ Frontend/src/Components/Banner.jsx
@@ -32,7 +32,7 @@ function Banner() {
 
   return (
     <>
-      <div className='max-w-screen-2xl mx-auto md:px-20 px-4 flex flex-col md:flex-row dark:pt-8 dark:bg-black py-4'>
+      <div className='bg-white max-w-screen-2xl mx-auto md:px-20 px-4 flex flex-col md:flex-row dark:pt-8 dark:bg-black py-4'>
 
         {/* Left */}
         <div className='w-full md:w-1/2 mt-12 md:mt-32 order-2 md:order-1 '>
